acca8b3dc0239780ed7f9fa6acda22a8a52194d0
Shafee <shafeerehman57@gmail.com>
2025-03-29 22:06:02 +1300

    Initial Commit

---
+++ GraceAPI/GraceAPI.h
@@ -0,0 +1,18 @@
+//
+//  GraceAPI.h
+//  GraceAPI
+//
+//  Created by Shafee Rehman on 29/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GraceAPI.
+FOUNDATION_EXPORT double GraceAPIVersionNumber;
+
+//! Project version string for GraceAPI.
+FOUNDATION_EXPORT const unsigned char GraceAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraceAPI/PublicHeader.h>
+
+
